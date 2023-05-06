ac86fa61e5f1c217c993640fca689c6e7c45aeec
Md. Fahim Bhuiyan <67233830+md-fahim-bhuiyan@users.noreply.github.com>
2023-05-06 23:37:33 +0600

    Data Type Update and add Data Type text file

---
+++ booleanDataType.py
@@ -0,0 +1,23 @@
+# Assigning Boolean values to variables
+x = True
+y = False
+
+# Using comparison operators
+print("Using comparison operators:")
+print("2 == 2 Output:", 2 == 2)  # Output: True
+print("2 != 2 Output:", 2 != 2)  # Output: False
+print("4 < 3 Output:", 4 < 3)   # Output: False
+
+
+# Using logical operators
+print("\nUsing logical operators")
+print("x and y Output:", x and y)  # Output: False
+print(x or y)   # Output: True
+print(not x)    # Output: False
+
+z = 10
+
+if z > 5:
+    print("z is greater than 5: ", z > 5)
+else:
+    print("z is less than or equal to 5: ", z < 5)
