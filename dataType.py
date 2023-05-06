294f8daf1d200f9e3e715cada4c778ef4570cc2b
Md. Fahim Bhuiyan <67233830+md-fahim-bhuiyan@users.noreply.github.com>
2023-05-06 23:37:33 +0600

    Data Type Update and add Data Type text file

--- dataType.py
+++ dataType.py
@@ -3,18 +3,25 @@ number2 = 10.323
 value = True
 name = "Fahim"
 
-
-x = ["apple", "banana", "cherry"]
-
 print(type(number1))
 print(type(number2))
 print(type(name))
 print(type(value))
 
+#List
+x = ["apple", "banana", "cherry"]
 
 print(type(x))
 print(x)
 print(x[1])
 
+#new line
 
+print("\n")
 
+#Tupple
+
+y = ("Bangladesh", "Canada", "India", "Pakistan", "USA")
+print(type(y))
+print(y)
+print(y[4])
